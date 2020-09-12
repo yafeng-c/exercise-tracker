850c27f9cc8555657ea186c2380da670c3f47cc3
Y Chen <yafeng_chen@outlook.com>
2020-09-12 16:55:49 +0200

    Signed-off-by: Y Chen

---
+++ exercise-tracker-frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ExercisesList from "./components/ExercisesList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
